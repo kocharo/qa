6d67fdb16e7e0522031c3d0097a8a90d2dde42b0
=Konstantinos Charopoulos <=kocharo@me.com>
2019-01-06 12:12:15 +0200

    Prepopulate User and Questions tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Question;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class,3)->create()->each(function($u){
+            $u->questions()
+                ->saveMany(
+                    factory(App\Question::class,rand(1,5))->make()
+                );
+        });
+        
     }
 }
